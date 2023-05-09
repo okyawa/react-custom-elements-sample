4bd85035fa30d44809bad4090d59b39796214527
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-09 10:36:20 +0900

    reactコンポーネントをcustom elements化

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+class ReactCounter extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot === null) {
+      return;
+    }
+    ReactDOM.render(<App />, this.shadowRoot);
+  }
+
+  disconnectedCallback() {
+    if (this.shadowRoot === null) {
+      return;
+    }
+    ReactDOM.unmountComponentAtNode(this.shadowRoot);
+  }
+}
+
+customElements.define("react-app", ReactCounter);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
