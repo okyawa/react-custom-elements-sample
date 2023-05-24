c804fce2e3f7ed32a2087ccef34bfeae45c09780
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-24 14:00:41 +0900

    remove: reportWebVitalsの削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import { ReactShadowCounter } from './components/ReactShadowCounter';
-import reportWebVitals from './reportWebVitals';
 
 /**
  * Custom Elementsの登録
  */
 customElements.define('react-shadow-counter', ReactShadowCounter);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
