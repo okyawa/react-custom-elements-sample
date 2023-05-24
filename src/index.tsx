97206465e1d4b6ab827454f6e80f9dc2dac200cc
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-24 14:44:02 +0900

    add: ReactCounterをcustomElements.define()に追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import { ReactCounter } from './components/ReactCounter';
 import { ReactShadowCounter } from './components/ReactShadowCounter';
 
 /**
  * Custom Elementsの登録
  */
 customElements.define('react-shadow-counter', ReactShadowCounter);
+customElements.define('react-counter', ReactCounter);
